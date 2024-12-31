498060f9f0746b6612e6c5f72895ee0f737b41b9
Sébastien <sebastien@shyrogan.fr>
2024-12-31 18:54:51 +0100

    feat(m2m): compilation fonctionne

--- qcm.m2m/src/qcm/m2m/M2M.java
+++ qcm.m2m/src/qcm/m2m/M2M.java
@@ -1,5 +1,7 @@
 package qcm.m2m;
 
+import java.io.IOException;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.resource.XtextResourceSet;
@@ -13,8 +15,13 @@ public class M2M {
 	public static void main(String[] args) {
 		Injector injector = new DSLStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
-		Resource resource = resourceSet.createResource(URI.createURI("model/questionnaire.qcm"));
-		resource.load(resourceSet.getLoadOptions());
+		Resource resource = resourceSet.createResource(URI.createURI("model/model.qcm"));
+		try {
+			resource.load(resourceSet.getLoadOptions());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Questionnaire questionnaire = (Questionnaire) resource.getContents().get(0);
 		System.out.print(questionnaire);
 	}
